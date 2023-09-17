14161f36c08a6e1b966d50d547721458acea4436
jacopaghera <123456789+jacopaghera@gmail.com>
2023-09-17 19:26:50 +0200

    added the feature which lets you choose the number of squares the right way

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //script
 const container = document.querySelector('.container');
-const button = document.querySelector('button');
+const btn = document.querySelector('button');
 
 function makeSquares(number) {
     for (let i = 0; i<(number*number); i++) {
@@ -9,18 +9,17 @@ function makeSquares(number) {
     }
 }
 
-function createGrid () {
-    width = prompt("insert number of squares per side of the grid");
-    makeSquares(width);
-}
+btn.addEventListener('click', () => {
+    let number = prompt("insert number of squares per side of the grid");
+    if (number>100) {prompt("too big")};
+    makeSquares(number);
+    let squares = document.querySelectorAll('.gridSquares');
 
-button.addEventListener('click', createGrid());
+    console.log(squares);
 
-let squares = document.querySelectorAll('.gridSquares');
-console.log(squares);
-
-squares.forEach(element => element.addEventListener('mouseover', () => {
+    squares.forEach(element => element.addEventListener('mouseover', () => {
     element.style.backgroundColor = '#0000FF';
 })
 );
+});
 
