50f6ece038ab1e21f3429d10b3f31e5c7884f2f0
jacopaghera <123456789+jacopaghera@gmail.com>
2023-09-17 20:31:44 +0200

    added an alorithm for which when you tap the button the second time the page reloads to avoid having too much squares

--- script.js
+++ script.js
@@ -2,24 +2,27 @@
 const container = document.querySelector('.container');
 const btn = document.querySelector('button');
 
+btn.addEventListener('click', () => {
+    if (document.querySelectorAll('.gridSquares').length > 0) {window.location.reload()}
+        let number = prompt("insert number of squares per side of the grid");
+        if (number>100) {prompt("too big")};
+        makeSquares(number);
+        let squares = document.querySelectorAll('.gridSquares');
+        console.log(squares);
+
+        squares.forEach(element => {
+        element.addEventListener('mouseover', () => {
+        element.style.backgroundColor = '#0000FF';
+        })
+        let width = 700/number;
+        element.style.flex = `1 0 ${width}px`;
+    })
+});
+
 function makeSquares(number) {
     for (let i = 0; i<(number*number); i++) {
         let row = document.createElement('div');
         container.appendChild(row).className = 'gridSquares';
     }
 }
-
-btn.addEventListener('click', () => {
-    let number = prompt("insert number of squares per side of the grid");
-    if (number>100) {prompt("too big")};
-    makeSquares(number);
-    let squares = document.querySelectorAll('.gridSquares');
-
-    console.log(squares);
-
-    squares.forEach(element => element.addEventListener('mouseover', () => {
-    element.style.backgroundColor = '#0000FF';
-})
-);
-});
 
